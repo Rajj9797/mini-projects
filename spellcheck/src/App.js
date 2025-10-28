b99d41f0af647ab3a4e70ed78b13fb1635edae0b
Raj Janrao <rajjanrao@Rajs-MacBook-Air.local>
2025-10-29 00:31:41 +0530

    Spellcheck created

---
+++ spellcheck/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SpellCheckApp from './Spellcheck';
+
+function App() {
+  return (
+    <div className="App">
+      <SpellCheckApp />
+    </div>
+  );
+}
+
+export default App;
